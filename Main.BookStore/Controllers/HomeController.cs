8f8b086dc44da519eaa7689d736b36610e216248
Hvirus777 <hvirus777@gmail.com>
2022-10-04 17:30:14 +0530

    Adding MVC into Application

---
+++ Main.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Main.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hey its from Home Controller!";
+        }
+    }
+}
