292603c2ccba98c94357da3edbd5ed049de8b5f1
Hvirus777 <hvirus777@gmail.com>
2022-10-05 11:52:59 +0530

    Added navigation Bar and ContactUs View

--- Main.BookStore/Controllers/HomeController.cs
+++ Main.BookStore/Controllers/HomeController.cs
@@ -8,8 +8,12 @@ namespace Main.BookStore.Controllers
         public ViewResult Index()
         {
             return View();
-        } 
+        }
         public ViewResult AboutUs()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
         {
             return View();
         }
