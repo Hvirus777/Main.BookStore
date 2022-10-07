ad503e8b626779b6ae64211819c63c6cd4ab5934
Hvirus777 <hvirus777@gmail.com>
2022-10-07 10:46:56 +0530

    Custom Tag Helper

---
+++ Main.BookStore/Helper/CustomEmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Main.BookStore.Helper
+{
+    public class CustomEmailTagHelper : TagHelper
+    {
+
+        public string MyEmail { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            output.TagName = "a";
+            //output.Attributes.SetAttribute("href", "mailto:hvirus777@gmail.com");
+
+            //Passing value from View helper dynamically 
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");
+            output.Content.SetContent("my-email");
+        }
+    }
+}
