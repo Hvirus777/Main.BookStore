fa665fd7a95dd13397bb84eefdc552fbd3b803cb
Hvirus777 <hvirus777@gmail.com>
2022-10-07 11:11:53 +0530

    Custom Tag Helpers

---
+++ Main.BookStore/Helper/BigTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+
+
+namespace Main.BookStore.Helper
+{
+
+    //this will work using 'AND' functionality. eg. Target tags whos tag AND attribute is 'big'
+    // [HtmlTargetElement("big", Attributes = "big")]
+
+    //this will work using 'OR' functionality. ForEg. Target tags whos tag is big OR target tags whos attribute is 'big'
+    [HtmlTargetElement("big")]
+    [HtmlTargetElement(Attributes = "big")]
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big"); 
+        }
+    }
+}
