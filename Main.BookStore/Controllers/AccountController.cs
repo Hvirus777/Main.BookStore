eb66f5ecbefdf4ed82d58e40bebf191d1a5ec988
Hvirus777 <hvirus777@gmail.com>
2022-10-08 17:02:06 +0530

    Created user registration form

---
+++ Main.BookStore/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Main.BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.BookStore.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+        [Route("sign-up")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost("sign-up")]        
+        public IActionResult Signup(SignUpUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                ModelState.Clear();
+            }
+            else
+            {
+
+            }
+
+
+            return View();
+        }
+    }
+}
