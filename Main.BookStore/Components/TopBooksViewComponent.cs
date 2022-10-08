5b85a43b3f5ac84639144348790725a61b4d6730
Hvirus777 <hvirus777@gmail.com>
2022-10-08 10:44:45 +0530

    View components in asp.net core

---
+++ Main.BookStore/Components/TopBooksViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Main.BookStore.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
